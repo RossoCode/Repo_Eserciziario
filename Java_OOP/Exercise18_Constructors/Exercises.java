939078b5210ff9a59d0d1b705f2d1137f81e7e39
lorenzo <rossolars@gmail.com>
2023-02-23 10:45:20 +0100

    inserimenti diciottessimo esercizio

--- Java_OOP/Exercise18_Constructors/Exercises.java
+++ Java_OOP/Exercise18_Constructors/Exercises.java
@@ -11,7 +11,6 @@ public class Exercises {
         exercise2();
     }
 
-
     /**
      *  1: Create a constructor inside Student that takes 'name' and 'age'
      *
@@ -19,27 +18,30 @@ public class Exercises {
      *
      *     Add a print statement to the constructor at the start saying "Constructing!"
      *     Add a print statement to the constructor at the start saying "Constructed!"
-     *
      */
     private static void exercise1() {
         System.out.println("Exercise 1:");
         List<String> studentNames = Arrays.asList("Alice", "Bob", "Charlie");
         List<Integer> studentAges = Arrays.asList(23, 31, 38);
         List<Student> students = new ArrayList<>();
-        Student student = new Student();
-
-        for (int i = 0; i < studentAges.size(); i++) {
+        Student student;
+        for (int i = 0; i < studentNames.size(); i++) {
             // Write create students
-            student.dataStudent(studentNames.get(i),studentAges.get(i));
-            System.out.println();
+            student = new Student(studentNames.get(i),studentAges.get(i));
+            students.add(student);
+            /**
+             * student.name = studentNames.get(i);
+            student.age = studentAges.get(i);
+            System.out.println("the name is "+ student.name + " and the age is " + student.age);
+             */
         }
-
+        System.out.println(students);
     }
 
     /**
      * 2: Add 3 more constructors to student, remember you need to change the parameters
      *    a constructor accepts in order to allow multiple.
-     *
+     *    -------------------------------------------------
      *    a Constructor that takes only a student's age
      *    a Constructor that takes only a student's name
      *    a Constructor that does not take any parameters
@@ -48,11 +50,11 @@ public class Exercises {
         System.out.println("\nExercise 2:");
 
         // Once you've added the correct constructors uncomment the code below, there should be errors!
-//         List<Student> students = Arrays.asList(
-//         new Student("Alice", 33),
-//         new Student("Bob"),
-//         new Student(33),
-//         new Student()
-//        );
+         List<Student> students =Arrays.asList(
+                                               new Student("Alice", 33),
+                                               new Student("Bob"),
+                                               new Student(33),
+                                               new Student()
+                                              );
     }
 }
--- Java_OOP/Exercise18_Constructors/Student.java
+++ Java_OOP/Exercise18_Constructors/Student.java
@@ -3,13 +3,44 @@ package Java_OOP.Exercise18_Constructors;
 public class Student {
     String name;
     int age;
-    void dataStudent(String n, int a) {
-    System.out.println("Constructing!");
-        name = n;
-        System.out.println(n);
-        age = a;
-        System.out.println(a);
-    System.out.println("Constructed!");
+     Student(String name, int age) {
+              System.out.println();
+         System.out.println("Constructing!");
+            this.name = name;
+            this.age = age;
+              System.out.println("the name is "+ name + " and the age is " + age);
+         System.out.println("Constructed!");
+              System.out.println();
+    }
+     Student(String name) {
+        System.out.println();
+        System.out.println("Constructing!");
+        this.name = name;
+        System.out.println("the name is "+ name );
+        System.out.println("Constructed!");
+        System.out.println();
+    }
+     Student(int age) {
+        System.out.println();
+        System.out.println("Constructing!");
+        this.age = age;
+        System.out.println("the age is " + age);
+        System.out.println("Constructed!");
+        System.out.println();
+    }
+     Student() {
+        System.out.println();
+        System.out.println("Constructing!");
+        System.out.println("Constructed!");
+        System.out.println();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
 
