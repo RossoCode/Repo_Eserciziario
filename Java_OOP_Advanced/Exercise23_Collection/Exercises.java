433445eb3a927881cbbf599d166bceaa4edd2c77
lorenzo <rossolars@gmail.com>
2023-03-08 15:52:35 +0100

    svolgimento 23° esercizio

--- Java_OOP_Advanced/Exercise23_Collection/Exercises.java
+++ Java_OOP_Advanced/Exercise23_Collection/Exercises.java
@@ -20,7 +20,9 @@ public class Exercises {
         List<String> list = new ArrayList<>(Arrays.asList("a", "b", "c", "a", "b", "c", "a", "b", "c"));
 
         // Your code here
-
+//        Set<String> setList = new HashSet<>(list);
+//        list = new ArrayList<>(setList);
+        list = new ArrayList<>(new HashSet<>(list));
         if (list.size() == 3) {
             System.out.println("Exercise 1: Success");
         } else {
@@ -36,26 +38,36 @@ public class Exercises {
         // Your code here
         List<String> list = Arrays.asList("apple", "banana", "apple", "orange", "kiwi", "banana");
         Set<String> set = Set.of("apple", "banana");
-        // toSet(list); <-- uncomment
-        // toSet(set); <-- uncomment
+        toSet(list);
+        toSet(set);
     }
 
     // Write your method for exercise 2 here
-
+    public static Set  toSet(Collection collection) {
+            return new HashSet(collection);
+    }
 
     /**
      * 3: Follow the instructions in the comments
      */
     private static void exercise3() {
+        System.out.println("Exercise 3:");
         String str = "the fox and the lazy dog saw another fox with another not lazy dog";
         List<String> words = Arrays.stream(str.split(" ")).toList();
         Map<String, Integer> wordCount = new HashMap<>();
-
         // Write code to count the number of occurrences of each word in the String `words`
         // and store the counts in the Map `wordCount`, note you will need to first get the previous value
         // from the Map and then increment it by 1, if no previous value exists, use 0 as the previous value.
 
         // Your code here
+        for ( String word: words ){
+            if(wordCount.containsKey(word))
+            {wordCount.put(word, wordCount.get(word) + 1);
+        } else {
+                wordCount.put(word, 1);
+            }
+        }
+        System.out.println(wordCount);
     }
 
     /**
@@ -65,40 +77,40 @@ public class Exercises {
      *    and explain why you chose that data structure.
      *
      *    1 - You want to store the students in order of their arrival
-     *    Answer:
+     *    Answer: List
      *
      *    2 - You want to store the students in a class and their grades
-     *    Answer:
+     *    Answer: Map
      *
      *    3 - You want to store the countries in the world in order of age
-     *    Answer:
+     *    Answer: Set
      *
      *    4 - You want to store the products available for purchase in an online store, along with their corresponding prices.
-     *    Answer:
+     *    Answer: Map
      *
      *    5 - You want to store the employees at a company in the order you added them
-     *    Answer:
+     *    Answer: List
      *
      *    6 - You want to store the cities in a particular state, along with their populations.
-     *    Answer:
+     *    Answer: Map
      *
      *    7 - You want to store the books in a library, along with their authors and publication dates.
-     *    Answer:
+     *    Answer: Map
      *
      *    8 - You want to store the courses offered at a university, along with the professors teaching each course.
-     *    Answer:
+     *    Answer: Map
      *
      *    9 - You want to store the movies in a movie rental store, along with their ratings and release dates.
-     *    Answer:
+     *    Answer: List
      *
      *    10 - You want to store the flights available for booking on an airline, along with their destinations and departure times.
-     *    Answer:
+     *    Answer: List
      *
      *    11 - You want to store the articles on a news website, along with their titles and publication dates.
-     *    Answer:
+     *    Answer: List
      *
      *    12 - You want to store the songs in a music streaming service, along with their artists and album names.
-     *    Answer:
+     *    Answer: List
      */
 
 }
