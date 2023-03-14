a635a4d54f65afe56d6a9d8b3b52ec1b8d37b856
lorenzo <rossolars@gmail.com>
2023-03-14 17:19:37 +0100

    svolgimento 24° esercizio

--- Java_OOP_Advanced/Exercise24_Reading_and_writing_to_a_file/Exercises.java
+++ Java_OOP_Advanced/Exercise24_Reading_and_writing_to_a_file/Exercises.java
@@ -1,15 +1,22 @@
 package Java_OOP_Advanced.Exercise24_Reading_and_writing_to_a_file;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.Files.writeString;
+
 
 public class Exercises {
 
-    private static final Path MY_PATH = Paths.get("file.txt");
-    private static final Path PATH_IN_A_FOLDER = Paths.get("mydirectory/file.txt");
+    private static final Path MY_PATH = Paths.get("Java_OOP_Advanced/Exercise24_Reading_and_writing_to_a_file/file.txt");
+    private static final Path PATH_IN_A_FOLDER = Paths.get("directory/file.txt");
 
     public static void main(String[] args) {
         exercise1();
@@ -33,11 +40,13 @@ public class Exercises {
                 """;
 
         try {
+            Files.deleteIfExists(MY_PATH);
             Files.createFile(MY_PATH);
 
             // Your code here
-            FileWriter fileWriter = new FileWriter("myfile");
-            fileWriter.write(myString);
+            Files.writeString(MY_PATH, myString);
+            System.out.println("Success");
+
         } catch (Exception exception) {
             System.err.println("There was an error!");
             exception.printStackTrace();
@@ -54,6 +63,13 @@ public class Exercises {
         System.out.println("\nExercise 2: ");
         try {
             // Your code here
+            FileReader reader = new FileReader("Java_OOP_Advanced/Exercise24_Reading_and_writing_to_a_file/file.txt");
+            int data = reader.read();
+            while (data != -1)
+            {
+                System.out.print((char) data);
+                data = reader.read();
+            }
         } catch (Exception exception) {
             System.err.println("There was an error!");
             exception.printStackTrace();
@@ -73,6 +89,12 @@ public class Exercises {
 
         try {
             // Your code here
+            BufferedReader lineReader = new BufferedReader(new FileReader("Java_OOP_Advanced/Exercise24_Reading_and_writing_to_a_file/file.txt"));
+            int numLines = 0;
+            BufferedReader reader;
+            while (lineReader.readLine() != null) numLines++;
+            lineReader.close();
+            System.out.println("has " + numLines + " line");
         } catch (Exception exception) {
             System.err.println("There was an error!");
             exception.printStackTrace();
@@ -91,6 +113,13 @@ public class Exercises {
         System.out.println("\nExercise 4: ");
         try {
             // Your code here
+            ArrayList<String> fileArray = new ArrayList<String>(List.of(Files.readString(MY_PATH).split(" ")));
+            int count = 0;
+            for (String Arrayword: fileArray) {
+                count++;
+            }
+            System.out.println("the number of words are " + count);
+            Files.delete(MY_PATH);
         } catch (Exception exception) {
             System.err.println("There was an error!");
             exception.printStackTrace();
@@ -104,11 +133,11 @@ public class Exercises {
      */
     private static void exercise5() {
         System.out.println("\nExercise 5: ");
-        Path myDirectoryPath = Path.of("mydirectory");
+        Path myDirectoryPath = Path.of("myDirectory");
 
         try {
             // Your code
-
+            Files.createDirectory(myDirectoryPath);
             Files.writeString(PATH_IN_A_FOLDER, "Why am I in a folder?");
         } catch (Exception exception) {
             System.err.println("There was an error!");
